0677db4aa7bfdf07477044af2f7418e286f0bae0
Hakan ERSU <hakanersu@gmail.com>
2026-02-08 23:09:29 +0300

    logo

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -31,7 +31,7 @@ function LandingNav({
     return (
         <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
             <div className="mx-auto flex h-16 max-w-6xl items-center justify-between px-4 sm:px-6">
-                <Link href="/" className="flex items-center gap-2">
+                <Link href="/" className="flex items-center">
                     <AppLogoIcon className="size-16 fill-foreground" />
                     <span className="text-lg font-semibold">{appName}</span>
                 </Link>
